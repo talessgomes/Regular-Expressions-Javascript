108214feabe2756acdfbcfa30362d7341ccf9432
Tales gomes <tales.gomes_sl@hotmail.com>
2022-10-05 20:58:42 -0300

    finalizado-exercicio-01

---
+++ ex01/index.js
@@ -0,0 +1,16 @@
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Change this line
+
+
+/*Neste desafio eu tenho que passar o resultado de true em test
+então myRegex vai ter o valor de /Hello/
+ */
+
+
+/* EXAMPLE
+
+let testStr = "freeCodeCamp";
+let testRegex = /Code/;
+testRegex.test(testStr); //true
+ */
